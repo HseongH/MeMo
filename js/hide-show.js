b1ed18f0bd8160bc899b3b29ec5ab7e7412752e9
unknown <hh4518@gmail.com>
2021-04-18 14:57:00 +0900

    카테고리 메뉴 수정

--- js/hide-show.js
+++ js/hide-show.js
@@ -1,44 +1,53 @@
 Memo.createObject('hideShow');
 
-Memo.hideShow.showOrHideElements = elem => {
-    elem.classList.toggle('hide');
+Memo.hideShow.toggleClass = (elem, apply) => {
+    elem.classList.toggle(apply);
 }
 
-Memo.hideShow.hideElements = elem => {
-    elem.classList.add('hide');
+Memo.hideShow.addClass = (elem, apply) => {
+    elem.classList.add(apply);
 }
 
-Memo.hideShow.showElements = elem => {
-    elem.classList.remove('hide');
+Memo.hideShow.removeClass = (elem, apply) => {
+    elem.classList.remove(apply);
 }
 
 // BUTTON
 Memo.hideShow.showMenuBtn = document.querySelector('.show-menu-button');
 Memo.hideShow.addMemoBtn = document.querySelector('.add-memo-button');
 Memo.hideShow.addMemoCloseBtn = document.querySelector('.memo-close-button');
-Memo.selectCategory = document.querySelector('.select-category');
+Memo.hideShow.selectCategory = document.querySelector('.select-category');
+Memo.hideShow.selectArrow = document.querySelector('.select-arrow');
 
 // ELEMENT
 Memo.hideShow.addMemo = document.querySelector('.section__add-memo');
 
 // CATEGORY MENU CONTROL
-Memo.categoryMenuArea.addEventListener('mouseover', () => {
-    Memo.hideShow.showElements(Memo.categoryMenu);
-});
-Memo.categoryMenuArea.addEventListener('mouseout', () => {
-    Memo.hideShow.hideElements(Memo.categoryMenu);
-});
+if (window.innerWidth > 700) {
+    Memo.categoryMenuArea.addEventListener('mouseover', () => {
+        Memo.hideShow.removeClass(Memo.categoryMenu, 'hide');
+    });
+    Memo.categoryMenuArea.addEventListener('mouseout', () => {
+        Memo.hideShow.addClass(Memo.categoryMenu, 'hide');
+    });
+} else {
+    Memo.hideShow.showMenuBtn.addEventListener('click', () => {
+        Memo.hideShow.toggleClass(Memo.categoryMenu, 'hide');
+    });
+}
 
 // ADD MEMO CONTROL
 Memo.hideShow.addMemoBtn.addEventListener('click', () => {
-    Memo.hideShow.showElements(Memo.hideShow.addMemo);
+    Memo.hideShow.removeClass(Memo.hideShow.addMemo, 'hide');
 });
 Memo.hideShow.addMemoCloseBtn.addEventListener('click', () => {
-    Memo.hideShow.hideElements(Memo.hideShow.addMemo);
-    Memo.hideShow.hideElements(Memo.categoryOptionCon);
+    Memo.hideShow.addClass(Memo.hideShow.addMemo, 'hide');
+    Memo.hideShow.addClass(Memo.categoryOptionCon, 'hide');
+    Memo.hideShow.removeClass(Memo.hideShow.selectArrow, 'select--activate');
 });
 
 // SELECT CATEGORY OPTION CONTROL
-Memo.selectCategory.addEventListener('click', () => {
-    Memo.hideShow.showOrHideElements(Memo.categoryOptionCon);
+Memo.hideShow.selectCategory.addEventListener('click', () => {
+    Memo.hideShow.toggleClass(Memo.categoryOptionCon, 'hide');
+    Memo.hideShow.toggleClass(Memo.hideShow.selectArrow, 'select--activate');
 });
